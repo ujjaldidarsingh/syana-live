fb428856aa1309644d1141de653d3c6b893ecbf7
Ujjal Didar Singh <ujjaldidarsingh.sekhon@gmail.com>
2026-05-22 11:38:52 -0400

    Configure Supabase backend

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.SYANA_LIVE_CONFIG = {
-  supabaseUrl: "",
-  supabaseAnonKey: "",
-  defaultSessionCode: "",
+  supabaseUrl: "https://dtxxqkgezrprmjslodzk.supabase.co",
+  supabaseAnonKey: "sb_publishable_lytM3LBFubiTVK8rov2C4g_rGI-KYzm",
+  defaultSessionCode: "RETREAT",
   appBaseUrl: "https://syana.us/",
 };
